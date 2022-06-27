6014b46e55cca8e41d3db666cca8fc97c0df886a
yfc1995 <441102979@qq.com>
2022-06-27 19:41:51 +0800

    配置antd按需加载

--- lib/generator/react/template/src/App.jsx
+++ lib/generator/react/template/src/App.jsx
@@ -1,20 +1,26 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 <% if (needRouter) {%>import routes from 'router/routerConfig'<%}%>
 <% if (needRouter) {%>import RouteWithSubRoutes from 'router/RouteWithSubRoutes'<%}%>
 
-import { <% if(projectType === 'Mobile'){ %>setMobileHtmlSize as setHtmlSize<% } else{ %>setPCHtmlSize as setHtmlSize<% } %>, debounce } from 'utils/selfAdaption';
+<% if (projectType === 'Mobile') {%>
+//import { setMobileHtmlSize as setHtmlSize, debounce} from 'utils/selfAdaption'
+<%}%>
+
+<% if (projectType === 'PC') {%>
+//import { setPCHtmlSize as setHtmlSize, debounce} from 'utils/selfAdaption'
+<%}%>
 
 function App() {
 
-  useEffect(() => {
-    const resize = debounce(setHtmlSize, 600, false);
-    setHtmlSize();
-    window.addEventListener('resize', resize);
-    return () => {
-      window.removeEventListener('resize', resize);
-    };
-  }, []);
-  
+  // useEffect(() => {
+  //   const resize = debounce(setHtmlSize, 600, false);
+  //   setHtmlSize();
+  //   window.addEventListener('resize', resize);
+  //   return () => {
+  //     window.removeEventListener('resize', resize);
+  //   };
+  // }, []);
+
   return (
     <div className="App">
     <% if (needRouter) {%>
