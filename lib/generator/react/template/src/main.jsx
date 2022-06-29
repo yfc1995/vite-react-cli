ec2f0c3c2acaad8052cf0769e9102ddd030f2792
yfc1995 <441102979@qq.com>
2022-06-29 09:48:33 +0800

    多语言

--- lib/generator/react/template/src/main.jsx
+++ lib/generator/react/template/src/main.jsx
@@ -4,15 +4,30 @@ import './index.css';
 import App from './App';
 <% if (needRedux) { %>import store from './store';
 import { Provider } from 'react-redux';<% } %>
+<% if (projectType === 'PC') { %>import zhCN from 'antd/lib/locale/zh_CN';
+import { ConfigProvider } from 'antd';
+<% } %>
+
 
 ReactDOM.render(
   <>
     <% if (needRedux) { %>
     <Provider store={store}>
-      <App />
+      <% if (projectType === 'PC') { %>
+      <ConfigProvider locale={zhCN}>
+        <App />
+      </ConfigProvider>
+      <%} else {%> 
+        <App /> 
+      <%} %>
     </Provider>
-    <%} else {%> 
-    <App /> 
+    <%} else if (projectType === 'PC') { %>
+        <ConfigProvider locale={zhCN}>
+          <App />
+        </ConfigProvider>
+        <%} else {%> 
+      <App /> 
+      <%} %>
     <%} %>
   </>,
   document.getElementById('root')
